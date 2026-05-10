521ba2f2d2d0ddfb091884d185291a3bd101b7a6
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-10 16:14:33 +0200

    renames encoder input

--- karamad/EfficientNetworkEncoder.py
+++ karamad/EfficientNetworkEncoder.py
@@ -5,5 +5,5 @@ class EfficientNetworkEncoder(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, images: torch.Tensor) -> torch.Tensor:
         pass
